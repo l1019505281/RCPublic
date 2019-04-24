fc0a5eccfa9e3683a14401609f1d1e402bf51f5f
lds <1019505281@qq.com>
2019-04-25 00:42:49 +0800

    Revert "Signed-off-by: lds <1019505281@qq.com>add queue to save data"
    This reverts commit 0bbf54987d534cf15713f6d74265512824e7792e.

--- MR1_v5_4-24/BSP/cylinder/cylinder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CYLINDER_H
-#define __CYLINDER_H
-
-#include "stm32f4xx.h"
-
-void cylinder_GPIO_init(void);
-
-#endif 
--- MR1_v5_4-24/BSP/limit_switch/limit_switch.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIMIT_SWITCH_H
-#define __LIMIT_SWITCH_H
-
-#include "stm32f4xx.h"
-
-void ls_GPIO_init(void);
-
-#endif
